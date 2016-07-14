9e0d4122e4bc31b1faf802291357332ba58168d8
jeffrey09hk <jeffreyhui04@hotmail.com>
2016-07-14 12:47:51 -0700

    Initial Commit

---
+++ Rankem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rankem
+//
+//  Created by Jeff Hui on 14/7/2016.
+//  Copyright © 2016 AffixGrp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
