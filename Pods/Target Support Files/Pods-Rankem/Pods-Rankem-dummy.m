104203ada405206567b5419f5bbfa1a4797a4c82
jeffrey09hk <Dante@Jeffs-MacBook-Air.local>
2016-07-20 15:12:02 -0700

    Got JSON

---
+++ Pods/Target Support Files/Pods-Rankem/Pods-Rankem-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Rankem : NSObject
+@end
+@implementation PodsDummy_Pods_Rankem
+@end
---
+++ Pods/Target Support Files/Pods-Rankem/Pods-Rankem-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_RankemVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_RankemVersionString[];
+
