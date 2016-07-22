1d4f833b339bb77799ceb8a988ccdc2df1744e26
jeffrey09hk <jeffreyhui04@hotmail.com>
2016-07-21 22:45:29 -0700

    finished MVP

--- Rankem/statViewController.swift
+++ Rankem/statViewController.swift
@@ -18,16 +18,19 @@ class statViewController: UIViewController {
     @IBOutlet weak var followingLabel: UILabel!
     @IBOutlet weak var followedByLabel: UILabel!
     @IBOutlet weak var likeCountLabel: UILabel!
+    @IBOutlet weak var topPicture: UIImageView!
+    @IBOutlet weak var rankingLabel: UILabel!
     
     
     var token: String = ""
     var userID: String = ""
     var fullName: String = ""
-    var fansCount: String = ""
+    var fansCount: Int = 0
     var followCount: String = ""
     var profilePicLink: String = ""
     
     var likeCount: Int = 0
+    var commentCount: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,11 +38,12 @@ class statViewController: UIViewController {
         getUserInfo()
         print("YOOOOO \(profilePicLink)")
         getMediaInfo()
+        imSuperPopular()
         
     }
     
     func getUserInfo(){
-        
+    
         let userInfoLink: String = "https://api.instagram.com/v1/users/self/?access_token=\(token)"
         
         Alamofire.request(.GET, userInfoLink).validate().responseJSON() { response in
@@ -49,13 +53,15 @@ class statViewController: UIViewController {
                 if let value = response.result.value {
                     let userData = JSON(value)
                     
+                    print("set fansCount and followCount")
+                    
                     self.userID = String(userData["data"]["id"])
                     self.fullName = String(userData["data"]["full_name"])
-                    self.fansCount = String(userData["data"]["counts"]["followed_by"])
+                    self.fansCount = userData["data"]["counts"]["followed_by"].intValue
                     self.followCount = String(userData["data"]["counts"]["follows"])
                     self.profilePicLink = String(userData["data"]["profile_picture"])
                     
-                    // Properties of the profile picture
+                    // Properties of the profile picture, make the picture circular
                     self.profilePic.layer.cornerRadius = self.profilePic.frame.size.height / 2
                     self.profilePic.layer.cornerRadius = self.profilePic.frame.size.width / 2
                     self.profilePic.clipsToBounds = true
@@ -64,7 +70,9 @@ class statViewController: UIViewController {
                     // Assign value to labels on page
                     self.fullnameLabel.text = self.fullName
                     self.followingLabel.text = self.followCount
-                    self.followedByLabel.text = self.fansCount
+                    self.followedByLabel.text = String(self.fansCount)
+                    
+                    self.imSuperPopular()
                     
                 }
             case .Failure(let error):
@@ -94,7 +102,7 @@ class statViewController: UIViewController {
     
     
     func getMediaInfo(){
-        // Sandbox mode only allow 20 pics
+        
         let userMediaLink: String = "https://api.instagram.com/v1/users/self/media/recent/?access_token=\(token)"
         
         Alamofire.request(.GET, userMediaLink).validate().responseJSON() { response in
@@ -104,12 +112,15 @@ class statViewController: UIViewController {
                 if let value = response.result.value {
                     let mediaData = JSON(value)
                     var highestLikeCount: Int = 0
-                    var pos: Int
+                    var pos: Int = 0
                     
+                    print("set likeCount")
+                    
+                    // Sandbox mode only allow 20 pics
                     for counter in 0...19{
-                        // get the total like count
+                        // get the total like and comment count
                         self.likeCount += mediaData["data"][counter]["likes"]["count"].intValue
-                        
+                        self.commentCount += mediaData["data"][counter]["comments"]["count"].intValue
                         
                         // Find the picture with the highest amount of likes
                         if mediaData["data"][counter]["likes"]["count"].intValue > highestLikeCount{
@@ -117,14 +128,79 @@ class statViewController: UIViewController {
                             pos = counter
                         }
                         
-                    }
+                    } // end of for loops
+                    
                     self.likeCountLabel.text = String(self.likeCount)
+                    
+                    // get the thumbnail of the most liked picture and link to the imgView
+                    let topPicLink = String(mediaData["data"][pos]["images"]["thumbnail"]["url"])
+                    
+                    statViewController.loadImageFromUrl(topPicLink, view: self.topPicture)
+                    self.topPicture.layer.cornerRadius = self.profilePic.frame.size.height / 2
+                    self.topPicture.layer.cornerRadius = self.profilePic.frame.size.width / 2
+                    self.topPicture.clipsToBounds = true
+                    
+                    self.imSuperPopular()
+                    
                 }
             case .Failure(let error):
                 print(error)
             }
-    
         } // end of alamofire
+    } // end of getMediaInfo
+    
+    func imPopular(){
+        
+        // source: https://paragonie.com/blog/2015/06/quantifying-popularity-in-real-time-for-high-volume-websites
+        
+        print("calculate score")
+        
+        // 1 comment is worth 2 likes
+        let weightedComments = self.commentCount * 2
+        
+        // the average score for a particular item
+        let itemAvgScore = (weightedComments + self.likeCount) / 20
+        
+        let voters = self.likeCount + weightedComments
+        
+        let score: Int = ((itemAvgScore * voters) + (self.fansCount * 1 )) / (voters + 1)
+        
+        print("the weighted comments count: \(weightedComments)")
+        
+        self.rankingLabel.text = String(score)
     
     }
+    
+    
+    func imSuperPopular(){
+        
+        let weightedComments = self.commentCount * 5
+        let weightedFans = self.fansCount * 10
+        
+        var score: Int = weightedComments + weightedFans + self.likeCount
+        self.rankingLabel.text = String(score)
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 } // end of class
